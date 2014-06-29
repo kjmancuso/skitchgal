164f64e46ae14461f1548bad4480655e49fa7a40
Kevin Mancuso <kevin.mancuso@rackspace.com>
2014-06-29 17:46:48 +0000

    Cleanup a bit

--- gal.py
+++ gal.py
@@ -1,60 +1,35 @@
+import logging
 import os
-import ConfigParser
+
+import flask_bootstrap
 
 from PIL import Image
 from flask import Flask, render_template, redirect, request
+from configobj import ConfigObj
 
-config = ConfigParser.ConfigParser()
-config.read('gal.conf')
-imgdir = config.get('gal', 'imgdir')
-thumbdir = config.get('gal', 'thumbdir')
-urlbase = config.get('gal', 'urlbase')
-thumbprefix = config.get('gal', 'thumbprefix')
-thumbsize = config.get('gal', 'thumbsize')
-allowed_extensions = str.split(config.get('gal', 'allowed_extensions'), ' ')
-num_per_row = config.getint('gal', 'num_per_row')
-bootstrapurl = config.get('gal', 'bootstrapurl')
+# Configs
+config = ConfigObj('gal.conf')
+allowed_extensions = str.split(config['allowed_extensions'], ' ')
+imgdir = config['imgdir']
+thumbdir = config['thumbdir']
+thumbprefix = config['thumbprefix']
+THUMBSIZE = 400, 400
 
+# App contructs
 app = Flask(__name__)
+flask_bootstrap.Bootstrap(app)
+app.logger.setLevel(logging.INFO)
+app.debug = True
 
 
+# Routes
 @app.route('/gal/')
 @app.route('/gal/index')
 def index():
     images = get_files(imgdir, allowed_extensions)
-    gen_thumbs(images, imgdir, thumbdir, thumbprefix, thumbsize)
+    gen_thumbs(images, imgdir, thumbdir, thumbprefix)
 
-    return render_template('index.html', urlbase=urlbase, images=images,
-                           numrow=num_per_row, bootstrapurl=bootstrapurl)
-
-
-def get_files(image_dir, allowed_extensions):
-    listing = os.listdir(image_dir)
-    images = []
-    for image in listing:
-        if os.path.splitext(image)[1] in allowed_extensions:
-            images.append(image)
-    images.sort(key=lambda x: os.path.getctime(image_dir + x))
-    images.reverse()
-
-    return images
-
-
-def gen_thumbs(images, imgdir, thumbdir, thumbprefix, thumbsize):
-    for image in images:
-        origfile = imgdir + image
-        thumbfile = thumbdir + thumbprefix + image
-        exists = os.path.exists(thumbfile)
-        if not exists:
-            print "Making thumb for %s" % origfile
-            mkthumb(origfile, thumbfile, thumbsize)
-
-
-def mkthumb(orig, thumbfile, thumbsize):
-    size = thumbsize
-    im = Image.open(orig)
-    im.thumbnail(size, Image.ANTIALIAS)
-    im.save(thumbfile)
+    return render_template('index.html', config=config, images=images)
 
 
 @app.route('/gal/delete/<image>', methods=['GET'])
@@ -63,12 +38,45 @@ def delete(image):
     filepath = imgdir + image
     thumbpath = thumbdir + 'thumb.' + image
     os.remove(filepath)
-    print "Deleting image %s" % filepath
+    app.logger.info('Deleting image %s' % filepath)
     os.remove(thumbpath)
-    print "Deleting thumb %s" % thumbpath
+    app.logger.info('Deleting thumb %s' % thumbpath)
+
     return redirect('/gal/index#' + anchor)
 
+# Functions
 
-if __name__ == "__main__":
+
+def get_files(image_dir, allowed_extensions):
+    listing = os.listdir(image_dir)
+    images = []
+    for image in listing:
+        if os.path.splitext(image)[1] in allowed_extensions:
+            images.append(image)
+    images.sort(key=lambda x: os.path.getmtime(image_dir + x))
+    images.reverse()
+
+    return images
+
+
+def gen_thumbs(images, imgdir, thumbdir, thumbprefix):
+    for image in images:
+        origfile = imgdir + image
+        thumbfile = thumbdir + thumbprefix + image
+        exists = os.path.exists(thumbfile)
+        if not exists:
+            app.logger.info('Making thumb for %s' % origfile)
+            mkthumb(origfile, thumbfile)
+
+
+def mkthumb(orig, thumbfile):
+    size = THUMBSIZE
+    print size
+    im = Image.open(orig)
+    im.thumbnail(size, Image.ANTIALIAS)
+    im.save(thumbfile)
+
+
+if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=9712)
